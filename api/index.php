99f990bf56c1cbc02732e4a780c3e0bcaeb489ee
nora hamioui <norahamioui761@gmail.com>
2026-03-31 19:13:24 +0200

    erreur

--- api/index.php
+++ api/index.php
@@ -9,10 +9,10 @@
     <h1>welcom noura</h1>
 
     <?php
-    echo("<a href="/ex1.php">exercice1</a>");
-    echo("<a href="/ex2.php">exercice2</a>");
-    echo("<a href="/Exe1.pdf">Rapport-Exe1</a>");
-    echo("<a href="/Exe2.pdf">Rapport-Exe2</a>");
+    echo("<a href='/ex1.php'>exercice1</a>");
+    echo("<a href='/ex2.php'>exercice2</a>");
+    echo("<a href='/Exe1.pdf'>Rapport-Exe1</a>");
+    echo("<a href='/Exe2.pdf'>Rapport-Exe2</a>");
 
 
 
