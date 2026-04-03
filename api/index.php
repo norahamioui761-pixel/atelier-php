f00450213ad03e6ae5a2b103f538438643e8190e
nora hamioui <norahamioui761@gmail.com>
2026-04-03 23:14:36 +0200

    my portfolio

--- api/index.php
+++ api/index.php
@@ -6,27 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>welcom noura</h1>
+    
 
     <?php
     echo("<a href='/ex1.php'>exercice1</a>");
     echo("<a href='/ex2.php'>exercice2</a>");
     echo("<a href='/Exe1.pdf'>Rapport-Exe1</a>");
     echo("<a href='/Exe2.pdf'>Rapport-Exe2</a>");
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
     ?>
 </body>
 </html>
