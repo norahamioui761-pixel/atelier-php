2e20acd1b2fdaf9096ad5eef45fb35ad3d3b076d
nora hamioui <norahamioui761@gmail.com>
2026-04-04 14:03:58 +0200

    noura el hamioui

--- api/index.php
+++ api/index.php
@@ -1,9 +1,3 @@
-  <?php
-    echo("<a href='/ex1.php'>exercice1</a>");
-    echo("<a href='/ex2.php'>exercice2</a>");
-    echo("<a href='/Exe1.pdf'>Rapport-Exe1</a>");
-    echo("<a href='/Exe2.pdf'>Rapport-Exe2</a>");
-    ?>
 
 <!DOCTYPE html>
 <html lang="fr">
